9222c794d6336e8233846c492841a565dac0c85b
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-11 20:01:39 -0700

    Initial commit

---
+++ src/main/java/edu/stanford/protege/webprotege/entity/WebprotegeEntityApplication.java
@@ -0,0 +1,13 @@
+package edu.stanford.protege.webprotege.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprotegeEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprotegeEntityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/stanford/protege/webprotege/entity/WebprotegeEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.stanford.protege.webprotege.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprotegeEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
